fcef870df7c8b7475ab888c6024b2efca4faa5b8
anubhabb66@gmail.com <anubhabb66@gmail.com>
2023-07-21 11:40:59 +0530

    initial commit

---
+++ LargeDataCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyExercise
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<int> data;
+        //constructor to initialize the collection with initial data
+
+        public LargeDataCollection(IEnumerable<int> initialData) 
+        {
+            data = new List<int>(initialData);
+            
+        }
+        //method to add elements in the collection
+        public void AddElement(int element)
+        {
+            data.Add(element);
+        }
+        //method to remove collection from the collection
+
+        public void RemoveElement(int element)
+        {
+            data.Remove(element);
+        }
+        //method to access the element in the collection
+
+        public int AccessElement(int index)
+        {
+            if(index >= 0 && index < data.Count)
+            {
+                return data[index];
+            }
+            throw new IndexOutOfRangeException("Index is out of range");
+
+        }
+        //implement the dispose method to release resources and free up memory
+
+        public void Dispose()
+        {
+            //release any unmanaged resource here 
+            //set the internal data structure to null to free up memory
+            data = null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(LargeDataCollection Collection = new LargeDataCollection(new int[] {1,2,3,4,5}))
+            {
+                //access and display elements
+                Console.WriteLine("Accessing elements: ");
+                for(int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Element at index {i}: {Collection.AccessElement(i)}");
+                }
+                //add and display element
+                Collection.AddElement(6);
+                Console.WriteLine($"Element at index 5: {Collection.AccessElement(5)}");
+                //remove the element and display the updated list
+                Collection.RemoveElement(3);
+                Console.WriteLine("Updated collection: ");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Element at index {i}: {Collection.AccessElement(i)}");
+                }
+                //after using 'largedatacollection' object, call the "dispose" method explicitly
+                Collection.Dispose();
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
